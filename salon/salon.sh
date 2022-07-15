5f5caa9a39505a5a845b36d1a14699799d25dea2
maanh96 <58045173+maanh96@users.noreply.github.com>
2022-07-15 18:24:01 +0700

    Create salon.sh

---
+++ salon/salon.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo "Welcome to My Salon, how can I help you?"
+
+SERVICES() {
+  # check if the argument pass to function
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  # list available services
+  AVAILABLE_SERVICES=$($PSQL "SELECT * FROM services ORDER BY service_id")
+  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+
+  # get service id & name
+  read SERVICE_ID_SELECTED
+  SERVICE_NAME_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  # if serivce not available
+  if [[ -z $SERVICE_NAME_SELECTED ]]
+  then
+    SERVICES "I could not find that service. What would you like today?"
+  else
+    # get customer info
+    echo "What's your phone number?"
+    read CUSTOMER_PHONE
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    #if customer doesn't exist
+    if [[ -z $CUSTOMER_ID ]]
+    then
+      # get new customer name
+      echo "I don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      # insert new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+      # get customer_id
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    else
+      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    fi
+    
+    # get appoinment info
+    echo "What time would you like your $SERVICE_NAME_SELECTED, $CUSTOMER_NAME?"
+    read SERVICE_TIME
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES ($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    if [[ $INSERT_APPOINTMENT_RESULT == "INSERT 0 1" ]]
+    then
+      echo "I have put you down for a $SERVICE_NAME_SELECTED at $SERVICE_TIME, $CUSTOMER_NAME."
+    fi
+  fi
+}
+
+SERVICES
